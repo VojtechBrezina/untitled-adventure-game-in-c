64bf11a0f826440bbe6612927d203d16b5238c93
Vojtech Brezina <vojta.brezina@gmail.com>
2022-02-03 10:34:42 +0100

    Introduce the game struct.

---
+++ src/game.h
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+// This struct holds the entire game state.
+typedef struct{
+
+} T_GAME;
+
+// Initialize the game at the very start.
+void GameInit(T_GAME *game);
+
+// Initialize the game from a sved state.
+void GameLoad(T_GAME *game, const char *name);
+
+// Save the game state.
+void GameSave(T_GAME *game, const char *name);
+
+// Clean all memory owned by the game when
+// no longer needed.
+void GameDone(T_GAME * game);
+
+// Process one command from the user and
+// dispose of it. Returns a logical value
+// stating whether the game should continue
+// or not for the main loop.
+int GameProcessCommand(char *command);
+
+#endif // GAME_H
