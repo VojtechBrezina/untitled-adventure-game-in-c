835731cff4bcd1b7a005363115282823161ef05b
Vojtech Brezina <vojta.brezina@gmail.com>
2022-02-05 20:59:51 +0100

    Create a dynamic array structure.

---
+++ src/dynamic_array.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "dynamic_array.h"
+
+void DynamicArrayInit(T_DYNAMIC_ARRAY *array){
+    array->size = 0;
+    array->capacity = 4;
+    array->data = malloc(sizeof(void *) * array->capacity);
+}
+
+void DynamicArrayAppend(T_DYNAMIC_ARRAY *array, void *value){
+    // If in debug mode, ensure the array hasn't been disposed.
+    assert(array->capacity != 0);
+
+    if(array->size == array->capacity){
+        array->capacity *= 2;
+        array->data = realloc(array->data, sizeof(void *) * array->capacity);
+    }
+
+    array->data[array->size++] = value;
+}
+
+void *DynamicArrayRemove(T_DYNAMIC_ARRAY *array, int index){
+    // Basic integrity checks:
+    assert(index < array->size && index >= 0);
+
+    void *value = array->data[index];
+    memmove(array->data + index, array->data + index - 1, array->size - index - 1);
+    array->size--;
+    return value;
+}
+
+void *DynamicArrayFastRemove(T_DYNAMIC_ARRAY *array, int index){
+    // Basic integrity checks:
+    assert(index < array->size && index >= 0);
+
+    void *value = array->data[index];
+    array->data[index] = array->data[--(array->size)];
+    return value;
+}
+
+void ArrayDone(T_DYNAMIC_ARRAY *array){
+    free(array->data);
+    array->data = NULL;
+    array->size = array->capacity = 0;
+}
---
+++ src/dynamic_array.h
@@ -0,0 +1,35 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+// A dynamically growing array implemented for arbitrary data.
+// The numbers 4 and 2 are taken from the .NET implementation
+// because I just cannot not let you know, that I remember
+// things like that. (correctly I hope)
+typedef struct{
+    int size;
+    int capacity;
+    void **data;
+} T_DYNAMIC_ARRAY;
+
+// Initialize an empty instance.
+void DynamicArrayInit(T_DYNAMIC_ARRAY *array);
+
+// Append a given value as the last element.
+void DynamicArrayAppend(T_DYNAMIC_ARRAY *array, void *value);
+
+// Remove an element from the array and return it to the user.
+// To dispose of it as necessary.
+void *DynamicArrayRemove(T_DYNAMIC_ARRAY *array, int index);
+
+// Remove the element by overwriting it with the last one and
+// decreasing the size. O(1), but disturbs the order of elements.
+void *DynamicArrayFastRemove(T_DYNAMIC_ARRAY *array, int index);
+
+// This function ensures all memory is returned and puts the array
+// in an invalid state. Not to be confused with clearing the array
+// which has to be performed manually as the arbitrary nature of the
+// data doesn't make anything more efficient possible. It is assumed
+// the data has already been destroyed by the caller.
+void ArrayDone(T_DYNAMIC_ARRAY *array);
+
+#endif // DYNAMIC_ARRAY_H
