ab87346c5945b7c23eb3ae594236474f6ae4aea9
Vojtech Brezina <vojta.brezina@gmail.com>
2022-02-05 20:14:55 +0100

    Expand the build script.

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ if [ ! -d build ]; then
     mkdir build
 fi
 
-gcc -Wall -pedantic -iquote . src/*.c -O2 -o build/game
+gcc -Wall -pedantic -DNDEBUG -iquote . src/*.c -O2 -o build/game && \
+gcc -g -fsanitize=address -iquote . src/*.c -Og -o build/game-d >/dev/null 2>/dev/null
+# ^ if succesful, create a debug build
