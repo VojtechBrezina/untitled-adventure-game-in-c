5598d89d6859c4c67d3bad8482ad4dc0ad175bf7
Vojtech Brezina <vojta.brezina@gmail.com>
2022-02-11 15:46:10 +0100

    Prepare main.c for the next stage.

--- src/main.c
+++ src/main.c
@@ -1,102 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
 #include "game.h"
 #include "dictionary.h"
+#include "util.h"
 
-// Holds the state of the main menu that
-// lets you start a new game or load an
-// existing one.
-typedef struct{
+#define SAVE_PATH ".saves"
 
-} T_MAIN_MENU;
+// Loads the saves file and grabs all the save filenames
+// from it. If the file doesn't exist, it creates one with
+// no saves in it.
+T_DICTIONARY *listSaves(){
+    FILE *savesFile = fopen(SAVE_PATH, "r");
 
-void MainMenuInit(){
+    if(!savesFile){
+        FILE *created = fopen(SAVE_PATH, "w");
+        fprintf(created, "0\n");
+        fclose(created);
+        return NULL;
+    }
 
+    T_DICTIONARY *result = NULL;
+    int count = 0;
+    char *line = simpleGetLine(savesFile);
+    sscanf(line, "%d", &count);
+
+    for(int i = 0; i < count; i++){
+        char *line = simpleGetLine(savesFile);
+        DictionaryInsert(&result, line, NULL);
+        free(line);
+        while(strcmp(line = simpleGetLine(savesFile), "#GAME_END"))
+            free(line);
+        free(line);
+    }
+
+    return result;
 }
 
 int main(void){
-    // Follows a little utility for playing with the
-    // dictionary using the "/usr/share/dict/words"
-    // file. Thi is messy and serves no purpose in
-    // the actual project, but will be archived under
-    // the v0.0.1 tag.
+    printf("\nVitej v \"Untitled adventure game in c\" %s\n\n", GAME_VERSION);
 
-    printf(
-        "Vitej v testovacim programu. ("GAME_VERSION")\n"
-        "V pameti je slovnik inicializovany slovy z\n"
-        "/usr/share/dict/words Tento program slouzi\n"
-        "k interaktivnimu volani knihovnich funkci\n"
-        "nasledujicim zpusobem:\n"
-        "prikaz slovo\n"
-        "kde prikz je: s = search, g = get, i = insert, r = remove\n"
-        "Ctrl+D pro ukonceni.\n\n"
-    );
+    T_DICTIONARY *availableSaves = listSaves();
 
-    T_DICTIONARY *d = NULL;
-    char *l = NULL;
-    size_t n = 0, r;
-    int count = 0;
-    FILE *f = fopen("/usr/share/dict/words", "r");
-    while((r = getline(&l, &n, f)) != -1){
-        l[r - 1] = '\0';
-        DictionaryInsert(&d, l, NULL);
-        free(l);
-        l = NULL;
-        n = 0;
-        count++;
-        if(count % 1000 == 0)
-            DictionaryOptimize(&d);
-    }
-    fclose(f);
-    free(l);
-    n = 0;
+    T_LIST *savesList = DictionaryListKeys(&availableSaves);
+    while(ListHasItems(savesList))
+        printf("%s\n", (char *)ListRemove(savesList, 0));
+    free(savesList);
 
-    DictionaryOptimize(&d);
-
-    while((r = getline(&l, &n, stdin)) != -1){
-        n = 0;
-        l[r - 1] = '\0';
-        char cmd = l[0];
-        char *w = l + 1;
-        while(*w && isspace(*w))
-            w++;
-        switch (cmd){
-            case 's': case 'S':{
-                const char *res = DictionarySearch(&d, w)->key;
-                if(res) printf("> %s\n\n", res);
-                else printf("> NULL\n\n");
-                break;
-            }
-            case 'i': case 'I':
-                DictionaryInsert(&d, w, NULL);
-                printf("> OK\n\n");
-                break;
-            case 'g': case 'G':
-                printf("> %s\n\n", DictionaryGet(&d, w) ? "YES" : "NO");
-                break;
-            case 'r': case 'R':
-                DictionaryRemove(&d, w);
-                printf("> OK\n\n");
-                break;
-            default:
-                printf("> ?\n\n");
-                break;
-        }
-
-        free(l);
-        l = NULL;
-    }
-    free(l);
-
-    while(d)
-        DictionaryRemoveSomething(&d);
-
-
-    T_GAME *game;
-
-    // printf("\nVitej v \"Untitled adventure game in c\" %s\n\n", GAME_VERSION);
+    while(availableSaves)
+        DictionaryRemoveSomething(&availableSaves);
 
     return EXIT_SUCCESS;
 }
